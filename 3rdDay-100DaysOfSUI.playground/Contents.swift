b295f4c13a888dcbc2435f74521a74d60ccd46d0
BerkinSili <berkinsili@gmail.com>
2022-01-16 11:46:08 +0300

    Day 3

---
+++ 3rdDay-100DaysOfSUI.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let colors = ["Red", "Blue", "Green", "Yellow", "Brown", "Black", "White", "Blue", "Green"]
+
+print("Number of items in colors array = \(colors.count)")
+
+let uniqueColoros = Set(colors)
+print ("Number of unique items in colors array = \(uniqueColoros.count)")
