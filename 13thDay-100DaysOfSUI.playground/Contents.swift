2263690fa5a8b2d05987a58fa05b5abf7f755ffd
BerkinSili <berkinsili@gmail.com>
2022-01-30 17:34:56 +0300

    extensions & challenge day 13

--- 13thDay-100DaysOfSUI.playground/Contents.swift
+++ 13thDay-100DaysOfSUI.playground/Contents.swift
@@ -75,3 +75,98 @@ func getRandomBool() -> some Equatable {
 }
 
 print(getRandomNumber() == getRandomNumber())
+//print(getRandomNumber() == getRandomBool())
+
+
+//MARK: Extensions
+
+var quote = "   The truth is rarely pure and never simple   "
+
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+print(trimmed)
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    var lines: [String] {
+        self.components(separatedBy: .newlines)
+    }
+}
+
+let trimmed2 = quote.trimmed()
+
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+}
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
+let cAndP = Book(title: "Crime and Punishment", pageCount: 576)
+
+
+//MARK: Challange
+
+
+/*
+ Make a protocol that describes a building, adding various properties and methods, then create two structs, House and Office, that conform to it. Your protocol should require the following:
+
+ A property storing how many rooms it has.
+ A property storing the cost as an integer (e.g. 500,000 for a building costing $500,000.)
+ A property storing the name of the estate agent responsible for selling the building.
+ A method for printing the sales summary of the building, describing what it is along with its other properties.
+
+ */
+
+protocol Building {
+    var numberOfRooms: Int { get }
+    var cost: Int { get set }
+    var reaName: String { get set }
+    func printSummary()
+    var type: String { get }
+}
+
+struct House: Building {
+    let type = "house"
+    
+    let numberOfRooms: Int
+    
+    var cost: Int
+    
+    var reaName: String
+    
+    var numberOfBeds: Int
+}
+
+struct Office: Building {
+    let type = "office"
+
+    let numberOfRooms: Int
+    
+    var cost: Int
+    
+    var reaName: String
+    
+    var hasParkingLot: Bool
+}
+
+extension Building {
+    func printSummary() {
+        print("Talk to \(reaName) to buy this \(type) for $\(cost).")
+    }
+}
+
+let myHome = House(numberOfRooms: 3, cost: 3_000_000, reaName: "DK", numberOfBeds: 4)
+let myOffice = Office(numberOfRooms: 2, cost: 7_000_000, reaName: "KK", hasParkingLot: true)
+
+myHome.printSummary()
+myOffice.printSummary()
