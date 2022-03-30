1f8ca1d5198f35968ccc41e29c03d9257e6272be
BerkinSili <berkinsili@gmail.com>
2022-03-30 17:37:21 +0300

    Days 20-21-22, guess the flag

---
+++ 20-21-22thDay-100DaysOfSUI/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Berkin Sili on 30.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
